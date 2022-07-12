c58a0edcee1365d5c482113ba0cc689dcb43429e
Anders Sandstrom <anders.sandstrom@esss.se>
2022-07-12 13:04:14 +0200

    Build ok. No functionality

--- ecmc_plugin_timing/ecmcPluginTiming.c
+++ ecmc_plugin_timing/ecmcPluginTiming.c
@@ -28,13 +28,13 @@ extern "C" {
 
 #include "ecmcPluginDefs.h"
 #include "ecmcPluginClient.h"
-#include "ecmcGrblDefs.h"
-#include "ecmcGrblWrap.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+
+#define ECMC_PLUGIN_DBG_PRINT_OPTION_CMD "DBG="
 
 static int    lastEcmcError   = 0;
 static char*  lastConfStr     = NULL;
@@ -47,7 +47,7 @@ pthread_t tid;
  *  Return value other than 0 will be considered error.
  *  configStr can be used for configuration parameters.
  **/
-int grblConstruct(char *configStr)
+int timingConstruct(char *configStr)
 {
   // only allow one loaded module
   if(alreadyLoaded) {    
@@ -55,21 +55,20 @@ int grblConstruct(char *configStr)
   }
   alreadyLoaded = 1;
 
-  // create grbl object and register data callback
+  // create timing object and register data callback
   lastConfStr = strdup(configStr);
 
-  return createGrbl(lastConfStr, getEcmcSampleTimeMS());
+  return 0;
 }
 
 /** Optional function.
  *  Will be called once at unload.
  **/
-void grblDestruct(void)
+void timingDestruct(void)
 {  
   if(lastConfStr){
     free(lastConfStr);
   }
-  deleteGrbl();
 }
 
 /** Optional function.
@@ -78,76 +77,25 @@ void grblDestruct(void)
  *  this plugin to react on ecmc errors
  *  Return value other than 0 will be considered to be an error code in ecmc.
  **/
-int grblRealtime(int ecmcError)
+int timingRealtime(int ecmcError)
 {  
   lastEcmcError = ecmcError;
-  return realtime(ecmcError);
+  return 0;
 }
 
 /** Link to data source here since all sources should be availabe at this stage
  *  (for example ecmc PLC variables are defined only at enter of realtime)
  **/
-int grblEnterRT(){
-  return enterRT();
+int timingEnterRT(){
+  return 0;
 }
 
 /** Optional function.
  *  Will be called once just before leaving realtime mode
  *  Return value other than 0 will be considered error.
  **/
-int grblExitRT(void){
+int timingExitRT(void){
   return 0;
-}
-
-// Plc function for execute grbl code
-double grbl_set_execute(double exe) {
-  return setExecute((int)exe);
-}
-
-// Plc function for halt grbl
-double grbl_mc_halt(double halt) {
-  return setHalt((int)halt);
-}
-
-// Plc function for resume grbl
-double grbl_mc_resume(double halt) {
-  return setResume((int)halt);
-}
-
-// Plc function for reset grbl
-//double grbl_mc_reset(double halt) {
-//  return setReset((int)halt);
-//}
-
-// Plc function for reset grbl
-double grbl_get_busy() {
-  return getBusy();
-}
-
-// Plc function for reset grbl
-double grbl_get_parser_busy() {
-  return getParserBusy();
-}
-
-// Plc function for reset grbl
-double grbl_get_code_row_num() {
-  return getCodeRowNum();
-}
-
-double grbl_reset_error() {
-  return resetError();
-}
-
-double grbl_get_error() {
-  return getError();
-}
-
-double grbl_get_all_enabled() {
-  return getAllAxesEnabled();
-}
-
-double grbl_set_all_enable(double enable) {
-  return setAllAxesEnable(enable);
 }
 
 // Register data for plugin so ecmc know what to use
@@ -155,263 +103,25 @@ struct ecmcPluginData pluginDataDef = {
   // Allways use ECMC_PLUG_VERSION_MAGIC
   .ifVersion = ECMC_PLUG_VERSION_MAGIC, 
   // Name 
-  .name = "ecmcPluginGrbl",
+  .name = "ecmcPluginTiming",
   // Description
-  .desc = "grbl plugin for use with ecmc.",
+  .desc = "timing plugin for use with ecmc.",
   // Option description
-  .optionDesc = "\n    "ECMC_PLUGIN_DBG_PRINT_OPTION_CMD"<1/0>    : Enables/disables printouts from plugin, default = disabled (=0).\n"
-                "      "ECMC_PLUGIN_X_AXIS_ID_OPTION_CMD"<axis id>: Ecmc Axis id for use as grbl X axis, default = disabled (=-1).\n"
-                "      "ECMC_PLUGIN_Y_AXIS_ID_OPTION_CMD"<axis id>: Ecmc Axis id for use as grbl Y axis, default = disabled (=-1).\n"
-                "      "ECMC_PLUGIN_Z_AXIS_ID_OPTION_CMD"<axis id>: Ecmc Axis id for use as grbl Z axis, default = disabled (=-1).\n"
-                "      "ECMC_PLUGIN_SPINDLE_AXIS_ID_OPTION_CMD"<axis id>: Ecmc Axis id for use as grbl spindle axis, default = disabled (=-1).\n"
-                "      "ECMC_PLUGIN_AUTO_ENABLE_AT_START_OPTION_CMD"<1/0>: Auto enable the linked ecmc axes autmatically before start, default = disabled (=0).\n"
-                "      "ECMC_PLUGIN_AUTO_START_OPTION_CMD"<1/0>: Auto start g-code at ecmc start, default = disabled (=0).\n"
-  ,
+  .optionDesc = "\n    "ECMC_PLUGIN_DBG_PRINT_OPTION_CMD"<1/0>    : Enables/disables printouts from plugin, default = disabled (=0).\n",
   // Plugin version
   .version = ECMC_EXAMPLE_PLUGIN_VERSION,
   // Optional construct func, called once at load. NULL if not definded.
-  .constructFnc = grblConstruct,
+  .constructFnc = timingConstruct,
   // Optional destruct func, called once at unload. NULL if not definded.
-  .destructFnc = grblDestruct,
+  .destructFnc = timingDestruct,
   // Optional func that will be called each rt cycle. NULL if not definded.
-  .realtimeFnc = grblRealtime,
+  .realtimeFnc = timingRealtime,
   // Optional func that will be called once just before enter realtime mode
-  .realtimeEnterFnc = grblEnterRT,
+  .realtimeEnterFnc = timingEnterRT,
   // Optional func that will be called once just before exit realtime mode
-  .realtimeExitFnc = grblExitRT,
+  .realtimeExitFnc = timingExitRT,
   // PLC funcs
-  .funcs[0] =
-      { /*----grbl_set_execute----*/
-        // Function name (this is the name you use in ecmc plc-code)
-        .funcName = "grbl_set_execute",
-        // Function description
-        .funcDesc = "double grbl_set_execute(<exe>) :  Trigg execution of loaded g-code at positive edge of <exe>",
-        /**
-        * 7 different prototypes allowed (only doubles since reg in plc).
-        * Only funcArg${argCount} func shall be assigned the rest set to NULL.
-        **/
-        .funcArg0 = NULL,
-        .funcArg1 = grbl_set_execute,
-        .funcArg2 = NULL,
-        .funcArg3 = NULL,
-        .funcArg4 = NULL,
-        .funcArg5 = NULL,
-        .funcArg6 = NULL,
-        .funcArg7 = NULL,
-        .funcArg8 = NULL,
-        .funcArg9 = NULL,
-        .funcArg10 = NULL,
-        .funcGenericObj = NULL,
-      },
-  .funcs[1] =
-      { /*----grbl_mc_halt----*/
-        // Function name (this is the name you use in ecmc plc-code)
-        .funcName = "grbl_mc_halt",
-        // Function description
-        .funcDesc = "double grbl_mc_halt(<halt>) :  Halt grbl motion at positive edge of <halt>",
-        /**
-        * 7 different prototypes allowed (only doubles since reg in plc).
-        * Only funcArg${argCount} func shall be assigned the rest set to NULL.
-        **/
-        .funcArg0 = NULL,
-        .funcArg1 = grbl_mc_halt,
-        .funcArg2 = NULL,
-        .funcArg3 = NULL,
-        .funcArg4 = NULL,
-        .funcArg5 = NULL,
-        .funcArg6 = NULL,
-        .funcArg7 = NULL,
-        .funcArg8 = NULL,
-        .funcArg9 = NULL,
-        .funcArg10 = NULL,
-        .funcGenericObj = NULL,
-      },
-  .funcs[2] =
-      { /*----grbl_mc_resume----*/
-        // Function name (this is the name you use in ecmc plc-code)
-        .funcName = "grbl_mc_resume",
-        // Function description
-        .funcDesc = "double grbl_mc_resume(<resume>) : Resume halted grbl motion at positive edge of <resume>",
-        /**
-        * 7 different prototypes allowed (only doubles since reg in plc).
-        * Only funcArg${argCount} func shall be assigned the rest set to NULL.
-        **/
-        .funcArg0 = NULL,
-        .funcArg1 = grbl_mc_resume,
-        .funcArg2 = NULL,
-        .funcArg3 = NULL,
-        .funcArg4 = NULL,
-        .funcArg5 = NULL,
-        .funcArg6 = NULL,
-        .funcArg7 = NULL,
-        .funcArg8 = NULL,
-        .funcArg9 = NULL,
-        .funcArg10 = NULL,
-        .funcGenericObj = NULL,
-      },
-  .funcs[3] =
-      { /*----grbl_get_busy----*/
-        // Function name (this is the name you use in ecmc plc-code)
-        .funcName = "grbl_get_busy",
-        // Function description
-        .funcDesc = "double grbl_get_busy() :  Get grbl system busy (still executing motion code)",
-        /**
-        * 7 different prototypes allowed (only doubles since reg in plc).
-        * Only funcArg${argCount} func shall be assigned the rest set to NULL.
-        **/
-        .funcArg0 = grbl_get_busy,
-        .funcArg1 = NULL,
-        .funcArg2 = NULL,
-        .funcArg3 = NULL,
-        .funcArg4 = NULL,
-        .funcArg5 = NULL,
-        .funcArg6 = NULL,
-        .funcArg7 = NULL,
-        .funcArg8 = NULL,
-        .funcArg9 = NULL,
-        .funcArg10 = NULL,
-        .funcGenericObj = NULL,
-      },
-  .funcs[4] =
-      { /*----grbl_get_parser_busy----*/
-        // Function name (this is the name you use in ecmc plc-code)
-        .funcName = "grbl_get_parser_busy",
-        // Function description
-        .funcDesc = "double grbl_get_parser_busy() :  Get g-code parser busy.",
-        /**
-        * 7 different prototypes allowed (only doubles since reg in plc).
-        * Only funcArg${argCount} func shall be assigned the rest set to NULL.
-        **/
-        .funcArg0 = grbl_get_parser_busy,
-        .funcArg1 = NULL,
-        .funcArg2 = NULL,
-        .funcArg3 = NULL,
-        .funcArg4 = NULL,
-        .funcArg5 = NULL,
-        .funcArg6 = NULL,
-        .funcArg7 = NULL,
-        .funcArg8 = NULL,
-        .funcArg9 = NULL,
-        .funcArg10 = NULL,
-        .funcGenericObj = NULL,
-      },
-  .funcs[5] =
-      { /*----grbl_get_code_row_num----*/
-        // Function name (this is the name you use in ecmc plc-code)
-        .funcName = "grbl_get_code_row_num",
-        // Function description
-        .funcDesc = "double grbl_get_code_row_num() :  Get g-code row number currently preparing for exe.",
-        /**
-        * 7 different prototypes allowed (only doubles since reg in plc).
-        * Only funcArg${argCount} func shall be assigned the rest set to NULL.
-        **/
-        .funcArg0 = grbl_get_code_row_num,
-        .funcArg1 = NULL,
-        .funcArg2 = NULL,
-        .funcArg3 = NULL,
-        .funcArg4 = NULL,
-        .funcArg5 = NULL,
-        .funcArg6 = NULL,
-        .funcArg7 = NULL,
-        .funcArg8 = NULL,
-        .funcArg9 = NULL,
-        .funcArg10 = NULL,
-        .funcGenericObj = NULL,
-      },
-  .funcs[6] =
-      { /*----grbl_get_error----*/
-        // Function name (this is the name you use in ecmc plc-code)
-        .funcName = "grbl_get_error",
-        // Function description
-        .funcDesc = "double grbl_get_error() :  Get error code.",
-        /**
-        * 7 different prototypes allowed (only doubles since reg in plc).
-        * Only funcArg${argCount} func shall be assigned the rest set to NULL.
-        **/
-        .funcArg0 = grbl_get_error,
-        .funcArg1 = NULL,
-        .funcArg2 = NULL,
-        .funcArg3 = NULL,
-        .funcArg4 = NULL,
-        .funcArg5 = NULL,
-        .funcArg6 = NULL,
-        .funcArg7 = NULL,
-        .funcArg8 = NULL,
-        .funcArg9 = NULL,
-        .funcArg10 = NULL,
-        .funcGenericObj = NULL,
-      },
-  .funcs[7] =
-      { /*----grbl_reset_error----*/
-        // Function name (this is the name you use in ecmc plc-code)
-        .funcName = "grbl_reset_error",
-        // Function description
-        .funcDesc = "double grbl_reset_error() :  Reset error.",
-        /**
-        * 7 different prototypes allowed (only doubles since reg in plc).
-        * Only funcArg${argCount} func shall be assigned the rest set to NULL.
-        **/
-        .funcArg0 = grbl_reset_error,
-        .funcArg1 = NULL,
-        .funcArg2 = NULL,
-        .funcArg3 = NULL,
-        .funcArg4 = NULL,
-        .funcArg5 = NULL,
-        .funcArg6 = NULL,
-        .funcArg7 = NULL,
-        .funcArg8 = NULL,
-        .funcArg9 = NULL,
-        .funcArg10 = NULL,
-        .funcGenericObj = NULL,
-      },
-  .funcs[8] =
-      { /*----grbl_get_all_enabled----*/
-        // Function name (this is the name you use in ecmc plc-code)
-        .funcName = "grbl_get_all_enabled",
-        // Function description
-        .funcDesc = "double grbl_get_all_enabled() :  Get all configured axes enabled.",
-        /**
-        * 7 different prototypes allowed (only doubles since reg in plc).
-        * Only funcArg${argCount} func shall be assigned the rest set to NULL.
-        **/
-        .funcArg0 = grbl_get_all_enabled,
-        .funcArg1 = NULL,
-        .funcArg2 = NULL,
-        .funcArg3 = NULL,
-        .funcArg4 = NULL,
-        .funcArg5 = NULL,
-        .funcArg6 = NULL,
-        .funcArg7 = NULL,
-        .funcArg8 = NULL,
-        .funcArg9 = NULL,
-        .funcArg10 = NULL,
-        .funcGenericObj = NULL,
-      },
-  .funcs[9] =
-      { /*----grbl_set_all_enable----*/
-        // Function name (this is the name you use in ecmc plc-code)
-        .funcName = "grbl_set_all_enable",
-        // Function description
-        .funcDesc = "double grbl_set_all_enable(enable) : Set enable on all configured axes.",
-        /**
-        * 7 different prototypes allowed (only doubles since reg in plc).
-        * Only funcArg${argCount} func shall be assigned the rest set to NULL.
-        **/
-        .funcArg0 = NULL,
-        .funcArg1 = grbl_set_all_enable,
-        .funcArg2 = NULL,
-        .funcArg3 = NULL,
-        .funcArg4 = NULL,
-        .funcArg5 = NULL,
-        .funcArg6 = NULL,
-        .funcArg7 = NULL,
-        .funcArg8 = NULL,
-        .funcArg9 = NULL,
-        .funcArg10 = NULL,
-        .funcGenericObj = NULL,
-      },
-
-  .funcs[10] = {0},  // last element set all to zero..
+  .funcs[0] = {0},  // last element set all to zero..
   // PLC consts
   .consts[0] = {0}, // last element set all to zero..
 };
---
+++ ecmc_plugin_timing/ecmcTimingDefs.h
@@ -0,0 +1,22 @@
+/*************************************************************************\
+* Copyright (c) 2019 European Spallation Source ERIC
+* ecmc is distributed subject to a Software License Agreement found
+* in file LICENSE that is included with this distribution. 
+*
+*  ecmcTimingDefs.h
+*
+*  Created on: Jul 12, 2022
+*      Author: anderssandstrom
+*
+\*************************************************************************/
+
+#ifndef ECMC_TIMING_DEFS_H_
+#define ECMC_TIMING_DEFS_H_
+
+// Options
+#define ECMC_PLUGIN_DBG_PRINT_OPTION_CMD "DBG_PRINT="
+#define ECMC_PLUGIN_ASYN_PREFIX          "plugin.timing"
+#define ECMC_CONFIG_FILE_COMMENT_CHAR    "#"
+#define ECMC_CONFIG_TIMING_CONFIG_CHAR     "$"
+
+#define ECMC_PLUGIN_TIMING_GENERAL_ERROR_CODE 0x100
